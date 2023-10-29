73087725964201868f25cd1ac2e79d31804ed408
jeevan-aj <ambrahamjeevan@gmail.com>
2023-10-29 11:54:13 +0530

    node hello world

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.end("hello");
+})
+server.listen(port,hostname,()=> {
+    console.log(`running at http://${hostname}:${port}/`)
+})
+
+// const http = require('http')
+// const server = http.createServer((req,res)=> {
+//     res.writeHead(200,{'Content-Type':'text/plain'})
+//     res.end("hello world")
+// }).listen(8080);
